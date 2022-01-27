53eb7cbb9cf3e3807f02bda8570b3526338d3af4
Hailey Magoline <magoline.hailey@gmail.com>
2022-01-27 17:36:25 -0600

    added favicon

--- src/App.js
+++ src/App.js
@@ -4,8 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
-       <h1>Hailey's Page</h1>
+    <header>
+      <h1>Welcome To My Portfolio</h1>
+        <nav>
+          <button>About</button>
+          <button>Portfolio</button>
+          <button>Resume</button>
+          <button>Contact</button>
+        </nav>
+    </header>
+    <div>
+      <h2>About</h2>
+    </div>
+       
+
+
 
     </div>
   );
