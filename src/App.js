82003b73261793c58ca7d19639430bb2dea414dc
Hailey Magoline <magoline.hailey@gmail.com>
2022-01-27 12:07:36 -0600

    test push for deployment issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     
        <h1>Hailey's Page</h1>
-      </header>
+
     </div>
   );
 }
